4eafbaac74396b726c7a3d1c914787f57e7eaffb
John Alexis Guerra Gómez <john.guerra@gmail.com>
2016-11-08 17:45:20 -0500

    Added a Procfile.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', function (req, res) {
   res.render("public/index.html");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
